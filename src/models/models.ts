a961e74c5241f89afce3a3bc42aba36dbe7ed7f3
Valentina-K <kvalentyna@hotmail.com>
2026-02-17 14:51:54 +0200

    add data, models and services

---
+++ src/models/models.ts
@@ -0,0 +1,64 @@
+export type LightCourseType = {
+    title: string, monthDuration: number
+}
+
+export type MiddleCourseType = {
+    title: string,
+    monthDuration: number,
+    hourDuration: number,
+    modules: string[]
+}
+
+export type CourseType = {
+    title: string,
+    monthDuration: number,
+    hourDuration: number,
+    modules: string[],
+    logo: string,
+    price: number,
+    rating: number,
+    avgResult: number
+}
+
+export type SimpsonType = {
+    name: string,
+    surname: string,
+    age: number,
+    info: string,
+    photo: string
+}
+
+export type FullUserType = {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: AddressType,
+    phone: string,
+    website: string,
+    company: CompanyType,
+}
+
+type AddressType = {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+        lat: string,
+        lng: string
+    }
+}
+
+type CompanyType = {
+    name: string,
+    catchPhrase: string,
+    bs: string
+}
+
+export type UserType = {name: string, age: number, status: boolean}
+
+export type LocationType = {
+    "title": string,
+    "type": string,
+}
---
+++ src/services/services.ts
@@ -0,0 +1,15 @@
+fetch("https://alerts.in.ua/assets/map.svg", {
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+});
+
+fetch("https://alerts.in.ua/assets/locations.json", {
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+});
