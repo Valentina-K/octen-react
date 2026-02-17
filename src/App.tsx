4b3d89e79f3fea96dfc3b1e9271246db97cee514
Valentina-K <kvalentyna@hotmail.com>
2026-02-17 15:28:29 +0200

    hw1 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css'
+import {coursesTitleArray} from "./data/arrays.ts";
 
 function App() {
 
 
-  return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </>
-  )
+    return (
+        <>
+            <ul className="bg-blue-300 border-zinc-700 p-10 rounded-2xl shadow-xl">
+                {coursesTitleArray.map((coursesTitle, index) => <li key={index} className="font-mono text-2xl text-gray-900 text-left mx-4">{coursesTitle}</li>)}
+            </ul>
+        </>
+    )
 }
 
 export default App
