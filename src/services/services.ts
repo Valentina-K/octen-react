5abb486e3cf28c2db18ecd5e8140b1e6bcaac280
Valentina-K <kvalentyna@hotmail.com>
2026-02-20 15:23:00 +0200

    update services

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,15 +1,2 @@
-fetch("https://alerts.in.ua/assets/map.svg", {
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": null,
-    "method": "GET",
-    "mode": "cors",
-    "credentials": "include"
-});
-
-fetch("https://alerts.in.ua/assets/locations.json", {
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": null,
-    "method": "GET",
-    "mode": "cors",
-    "credentials": "include"
-});
+const base_url = import.meta.env.VITE_BASE_URL;
+console.log(base_url);
