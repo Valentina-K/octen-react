9a96de56661a964b167a58e09f29c75635bde158
Valentina-K <kvalentyna@hotmail.com>
2026-02-27 10:52:16 +0200

    hw3-1.2 is done

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 export const Menu = () => {
     return (
-        <nav className="flex items-center justify-evenly w-full p-4 bg-lime-300">
+        <nav className="flex items-center justify-evenly w-full p-4 bg-lime-300 mb-10">
             <Link to={'users'}>Users</Link>
             <Link to={'posts'}>Posts</Link>
             <Link to={'comments'}>Comments</Link>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
 import {createRoot} from 'react-dom/client'
 import './index.css';
-import {RouterProvider} from "react-router";
-import {routes} from "./route/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Users} from "./components/Users/Users.tsx";
+import {Posts} from "./components/posts/Posts.tsx";
+import {Comments} from "./components/comments/Comments.tsx";
+import {Products} from "./components/products/Products.tsx";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>);
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path={'/octen-react'} element={<App/>}>
+                <Route path={'users'} element={<Users/>}/>
+                <Route path={'posts'} element={<Posts/>}/>
+                <Route path={'comments'} element={<Comments/>}/>
+                <Route path={'products'} element={<Products/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
+);
