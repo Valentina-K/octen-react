b174a89e91cbfb1b6733331c0eb77351d7da2640
Valentina-K <kvalentyna@hotmail.com>
2026-03-10 11:46:24 +0200

    fix menu styles

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import {Link, Outlet} from "react-router";
 export const MainLayout = () => {
     return (
         <>
-            <menu className={'bg-cyan-200 p-4 text-center w-full text-sm text-xl'}>
+            <menu className={'bg-cyan-200 p-4 text-center w-full text-xl'}>
                 <Link to={'users'} >Users</Link>
             </menu>
             <Outlet />
