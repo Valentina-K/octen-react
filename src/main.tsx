2bf2b58cf31006855d8ddd797515ca3b75d7cb82
Valentina-K <kvalentyna@hotmail.com>
2026-03-23 16:08:45 +0200

    Remove comment about RouterProvider rendering
    Removed commented line explaining the render method.

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import {routes} from "./routes/routes.tsx";
-//метод render задає <RouterProvider router={routes}/> з прописаним об'єктом routes
+
 createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>);
