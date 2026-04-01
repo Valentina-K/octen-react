664bb1cffdc36ddb6caf5e9a8ba8d12a4e6f59ac
Valentina-K <kvalentyna@hotmail.com>
2026-04-01 16:32:41 +0300

    hw7.1.3 is done

--- src/components/LeftBranch.tsx
+++ src/components/LeftBranch.tsx
@@ -1,7 +1,15 @@
 
 import {LeftBranchA} from "./LeftBranchA.tsx";
+import {useMemo} from "react";
 
 export const LeftBranch = () => {
+    //огортаємо цикл з багатьма ітераціями у useMemo, що запобігає ререндерингу дочірнього компоненту при ререндерингу LeftBranch
+    const memox = useMemo(() => {
+        for (let i = 0; i < 1000; i++) {
+            console.log(i);
+        }
+        return null;
+    }, []);
     return (
         <div>LeftBranch
 
--- src/components/RightBranch.tsx
+++ src/components/RightBranch.tsx
@@ -4,9 +4,9 @@ import {RightBranchA} from "./RightBranchA.tsx";
 export const RightBranch = () => {
     return (
         <div>RightBranch
-            <p>
+
                 <RightBranchA/>
-            </p>
+
         </div>
     );
 };
