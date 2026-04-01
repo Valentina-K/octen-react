abdb89ad3a0d4a062a55be6ae62a4837753978ab
Valentina-K <kvalentyna@hotmail.com>
2026-04-01 11:33:50 +0300

    add model

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import type {IUser} from "./models/IUser.ts";
+import {useFetch} from "./hooks/useFetch.tsx";
+import type {IUsersResponse} from "./models/IUsersResponse.ts";
 
 function App() {
-
+const {users} = useFetch<IUsersResponse>('https://dummyjson.com/users', { users: [], total: 0, limit: 0, skip: 0 }) as IUsersResponse;
 
   return (
       <>
-
+          {users.map((user: IUser) =>
+              <div key={user.id}>{user.id}: {user.firstName} {user.lastName}</div>
+          )}
       </>
   )
 }
