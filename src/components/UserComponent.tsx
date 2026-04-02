4465b314e74c90a6fd3a0f29a736ef00f787dfc4
Valentina-K <kvalentyna@hotmail.com>
2026-04-02 09:09:21 +0300

    hw7.1.4 is done, life cycle, useCallback

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,11 +1,20 @@
+
 import {type FC, memo} from "react";
-
-const UserComponent: FC<{ foo: () => void, arr: number[], item: { name: string } }> = memo(({arr, item}) => {
+//огортання компоненту у функцію memo запобігає повторному рендерингу компонента при ререндері
+//батьківського у випадку, якщо дані компонента остаються незмінними
+//хоча коипонент приймає пропс, він все одно не буде повторно рендериться, оскільки в батьківському
+//компоненті функція foo огорнута у useCallback
+export const UserComponent: FC<{ foo: () => void }> = memo(() => {
     console.log('user');
-    console.log(arr);
-
     return (
-        <div>{item.name}</div>
+        <div>user</div>
     );
 });
-export default UserComponent
+
+/*
+export const UserComponent: FC<{ foo: () => void }> =() => {
+    console.log('user');
+    return (
+        <div>user</div>
+    );
+};*/
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,19 +1,12 @@
 
-import UserComponent from "./UserComponent.tsx";
-import {useCallback, useMemo} from "react";
-import {useFetch} from "../hooks/useFetch.tsx";
+import {UserComponent} from "./UserComponent.tsx";
+import {useCallback, useEffect, useState} from "react";
 
 
 export const UsersComponent = () => {
     //отримуємо всіх юзерів
-    const users = useFetch();
-    //мемоїзуємо масив щоб запобігти ререндерингу дочірнього компоненту, оскільки масив - це об'єкт, а
-    //при ререндерингу компоненту UsersComponent він отримує нове посилання, яке передається пропсами в дочірній компонент
-    //і оскільки посилання нове, значить дочірній компонент ререндериться
-    const arr: number[] = useMemo(() => {
-        return [11, 22, 33];
-    }, []);
-
+    const [users, setUsers] = useState([]);
+    console.log(users);
     //мемоїзуємо фугкцію щоб запобігти ререндерингу дочірнього компоненту, оскільки функція - це об'єкт, а
     //при ререндерингу компоненту UsersComponent вона отримує нове посилання, яке передається пропсами в дочірній компонент
     //і оскільки посилання нове, значить дочірній компонент ререндериться
@@ -21,12 +14,23 @@ export const UsersComponent = () => {
         console.log('test');
     }, []);
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                setUsers(value);
+            });
+
+        return () => {
+            console.log('unsubscribe');
+        }
+
+    }, []);
+
 
     return (
         <div>users component
-            {
-                users.map(value => <UserComponent item={value} foo={foo} arr={arr}/>)
-            }
+            <UserComponent foo={foo}/>
         </div>
     );
 };
